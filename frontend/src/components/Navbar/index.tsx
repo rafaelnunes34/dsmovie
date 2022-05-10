62c6894dac5f5810de41846fc01aad3ef7288c74
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-05-10 07:57:20 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as GitHubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+
+    return(
+        <header>
+            <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/devsuperior">
+                <div className="dsmovie-contact-container">
+                    <GitHubIcon />
+                    <p className="dsmovie-contanct-link">/devsuperior</p>
+                </div>
+                </a>
+            </div>
+            </nav>
+      </header>
+    );
+
+}
+
+export default Navbar;
