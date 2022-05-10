b7d61572d8e6e82522b2bce1fef17d91c1b92f76
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-05-10 14:59:40 -0300

    Projeto Frontend

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,31 @@
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
+
 function Listing() {
     return (
-        <h1>Página de listagem</h1>
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
 
